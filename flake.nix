a4b952d15751a3940ee68fcd03e2b669a2831a84
retonlage <retonlage@retonlage.xyz>
2022-05-19 13:05:48 +0200

    Fix

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
           bin/ksql-run-class
         ];
         in
-        builtins.concatMap (bashScript: "substituteInPlace ${bashScript} --replace '#!/bin/bash' '${pkgs.bash}/bin/bash';") bashScript;
+        builtins.concatMap (bashScript: "substituteInPlace ${bashScript} --replace '#!/bin/bash' '${pkgs.bash}/bin/bash';") bashScripts;
       };
     };
     nixosModule = nixosModules.ksqldb;
